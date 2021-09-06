33cc229cf28d6358d859bb084b2d9e8ac8345e19
Alex <kolesnikaleksv@gmail.com>
2021-09-06 08:52:57 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,36 @@
+let number = 1;
+const NUmber = 5;
+
+number = 8;
+console.log(number);
+const obj = {
+    a: 10
+};
+obj.a = 11
+console.log(obj.a);
+// alert('hello world');
+// const answer = confirm('are you  eighteen years');
+// console.log(answer);
+
+// const quize = +prompt('are you here?', 'No');
+// console.log(typeof(quize));
+
+// const answers = [];
+// answers[0] = " hi";
+// answers[1] = prompt('how old are you?', '');
+// answers[2] = prompt('What is your name?', '');
+// answers[3] = prompt('do you have sex?', '');
+
+// console.log(typeof(answers));Hello ${answers[2]} you are too small for this site. you are ${answers[1]} years old
+// alert(``);
+
+let incr = 10,
+    decr = 10;
+
+    incr++;
+    ++decr;
+    console.log(incr,decr);
+    console.log(3*5 === 15);
+    let arg = 5;
+    console.log(`5 + ${arg}`);
+    console.log(`6 - ${3}`);
