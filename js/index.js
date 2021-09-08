1db064ad7290b7be14f6fd438d8fb70733350f6f
Alex <kolesnikaleksv@gmail.com>
2021-09-08 13:45:44 +0300

    The fourth task is completed

--- js/index.js
+++ js/index.js
@@ -2,11 +2,11 @@
 
 let numberOfFilms;
 
-function start() {
-    while(numberOfFilms == null || numberOfFilms == '' || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt('How many movies have you already watched?', '');
-    }
-}
+// function start() {
+//     while(numberOfFilms == null || numberOfFilms == '' || isNaN(numberOfFilms)) {
+//         numberOfFilms = +prompt('How many movies have you already watched?', '');
+//     }
+// }
 
 //start();
 
@@ -15,79 +15,113 @@ const personalMovieDB = {
     movies: {},
     actors: {},
     genres: [],
-    privat: false
+    privat: false,
+    start: function() {
+        while(numberOfFilms == null || numberOfFilms == '' || isNaN(numberOfFilms)) {
+            numberOfFilms = +prompt('How many movies have you already watched?', '');
+        }
+    },
+    showMyDB: function() {
+        if(personalMovieDB.privat == false) {
+            console.log(personalMovieDB);
+        }
+    },
+    writeYourGenres: function() {
+        for(let i = 0; i < 3; i++) {
+            let a = prompt(`What is your favorite genre under number?${i + 1}`, '');
+            if(a == null || a == '') {
+                i--;
+            } else {
+                personalMovieDB.genres[i] = a;
+            }
+        }
+        personalMovieDB.genres.forEach((element, index) => {
+            console.log(`Favorite genre # ${index + 1} - is ${element}`);
+        })
+    },
+    rememberMyFilms: function() {
+        for(let i = 0; i < 2; i++) {
+            let a = prompt('One of the last movies you watched?', ''),
+                b = prompt('How mach do you appreciate it?', '');
+                
+                if(a == "" || b == '' || a == null || b == null ||  a.length > 50 || b.length > 50){
+                    i--;
+                    console.log('fail');
+                } else {
+                    personalMovieDB.movies[a] = b;
+                }
+        }
+    },
+    personalLevelDetected: function() {
+        if(personalMovieDB.count <= 10 && personalMovieDB.count > 0) {
+            alert('You have watched too few movies.');
+        } else if(personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+            alert("You are a classic spectator.");
+        } else if(personalMovieDB.count >= 30) {
+            alert("You are a big fan of movies.")
+        } else {
+            alert("An error occurred!");
+        }
+    }
 };
-
-function showMyDB() {
-    if(personalMovieDB.privat == false) {
-        console.log(personalMovieDB);
+console.log(personalMovieDB.privat);
+function toggleVisibleMyDB() {
+    if(!personalMovieDB.privat) {
+        personalMovieDB.privat = true;
+    } else {
+        personalMovieDB.privat = false;
     }
 }
+toggleVisibleMyDB();
+console.log(personalMovieDB.privat);
+personalMovieDB.showMyDB();
+personalMovieDB.writeYourGenres();
 
-showMyDB();
+// function showMyDB() {
+//     if(personalMovieDB.privat == false) {
+//         console.log(personalMovieDB);
+//     }
+// }
 
-function writeYourGenres() {
-    for(let i = 0; i < 3; i++) {
-        personalMovieDB.genres[i] = prompt(`What is your favorite genre under number?${i + 1}`, '');
-    }
-}
+// showMyDB();
+
+// function writeYourGenres() {
+//     for(let i = 0; i < 3; i++) {
+//         personalMovieDB.genres[i] = prompt(`What is your favorite genre under number?${i + 1}`, '');
+//     }
+// }
 
 //writeYourGenres();
 
-function rememberMyFilms() {
-    for(let i = 0; i < 2; i++) {
-        let a = prompt('One of the last movies you watched?', ''),
-            b = prompt('How mach do you appreciate it?', '');
+// function rememberMyFilms() {
+//     for(let i = 0; i < 2; i++) {
+//         let a = prompt('One of the last movies you watched?', ''),
+//             b = prompt('How mach do you appreciate it?', '');
             
-            if(a == "" || b == '' || a == null || b == null ||  a.length > 50 || b.length > 50){
-                i--;
-                console.log('fail');
-            } else {
-                personalMovieDB.movies[a] = b;
-            }
-    }
-}
+//             if(a == "" || b == '' || a == null || b == null ||  a.length > 50 || b.length > 50){
+//                 i--;
+//                 console.log('fail');
+//             } else {
+//                 personalMovieDB.movies[a] = b;
+//             }
+//     }
+// }
 
 //rememberMyFilms();
 
-function personalLevelDetected() {
-    if(personalMovieDB.count <= 10 && personalMovieDB.count > 0) {
-        alert('You have watched too few movies.');
-    } else if(personalMovieDB.count > 10 && personalMovieDB.count < 30) {
-        alert("You are a classic spectator.");
-    } else if(personalMovieDB.count >= 30) {
-        alert("You are a big fan of movies.")
-    } else {
-        alert("An error occurred!");
-    }
-}
+// function personalLevelDetected() {
+//     if(personalMovieDB.count <= 10 && personalMovieDB.count > 0) {
+//         alert('You have watched too few movies.');
+//     } else if(personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+//         alert("You are a classic spectator.");
+//     } else if(personalMovieDB.count >= 30) {
+//         alert("You are a big fan of movies.")
+//     } else {
+//         alert("An error occurred!");
+//     }
+// }
 
 //personalLevelDetected();
 
 console.log(personalMovieDB);
 
-// OOP
-let john = {
-    health: 200,
-    armor: 400,
-    sayHello: function() {
-        console.log('Hello');
-    }
-}
-let soldier = {
-    health: 100
-}
-//soldier.__proto__ = john; // Outdated method
-Object.setPrototypeOf(soldier, john);
-
-console.log(soldier.armor);
-soldier.sayHello();
-// instead we have to use new mehtods
-//Object.create()        - create a prototype with a specific prototype
-//Object.getProrotypeOf()  - receives prototypes
-//Object.setPrototypeOf()  - obtaining prototypes
-//soldier.__proto__ = john; == Object.setPrototypeOf(soldier, john);
-// we create a new object joy that will be inherited from the object soldier
-const joy = Object.create(john);
-console.log(joy.health);
-joy.sayHello();
