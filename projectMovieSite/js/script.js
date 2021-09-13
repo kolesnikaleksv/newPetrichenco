fd469ce83c8042222fd84dd297bfcd5ecd57b835
Alex <kolesnikaleksv@gmail.com>
2021-09-13 18:10:51 +0300

    adding a draft of a task

--- projectMovieSite/js/script.js
+++ projectMovieSite/js/script.js
@@ -1,88 +1,96 @@
 'use strict';
 
-// const movieDB = {
-//     movies: [
-//         "Логан",
-//         "Лига справедливости",
-//         "Ла-ла лэнд",
-//         "Одержимость",
-//         "Скотт Пилигрим против..."
-//     ]
-// };
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
+};
 
-// let adv = document.querySelectorAll('.promo__adv img'),
-//     genre = document.querySelectorAll('.promo__genre'),
-//     promoBg = document.querySelectorAll('.promo__bg'),
-//     list = document.querySelectorAll('.promo__interactive-list'),
-//     movieName = list[0].querySelectorAll('.promo__interactive-item');
+let adv = document.querySelectorAll('.promo__adv img'),
+    genre = document.querySelectorAll('.promo__genre'),
+    promoBg = document.querySelectorAll('.promo__bg'),
+    list = document.querySelector('.promo__interactive-list'),
+    input = document.querySelector('.adding__input'),
+    trash = list.querySelectorAll('.delete'),
+    checkbox = document.querySelectorAll('.add input')[1],
+    btn = document.querySelector('.add button');
 
-// let arr = [];
-// for(let i = 0; i < movieDB.movies.length; i++) {
-//     arr[i] = movieDB.movies[i].toLowerCase();
-//     arr.sort();
+console.log(trash);
+// function delTrash() {
+//     console.log('hello');
 // }
-
-// arr.forEach((item, i) => {
-//     movieName[i].textContent = `${i + 1}. ${item}`;
-// });
-
-// adv.forEach(item => {
-//     item.remove();
-// });
-
-// genre[0].textContent = 'Драма';
-// promoBg[0].style.backgroundImage = 'url("img/bg.jpg")';
-
-
-
-// lesson working with events
-
-// let btn = document.querySelectorAll('.add button')[0],
-//     btns = document.querySelectorAll('.add');
-
-// // btn.addEventListener('click', (e) => {
-// //     btn.remove();
-// // });
-// let i = 0;
-// const deleteElement = function(e) {
-//     console.log(e.target);
-//     btn.remove()
-//     i++;
-//     btn.removeEventListener(); // remove the event listener after the first click
+trash.forEach(item => {
+    console.log(item);
+    item.style.cssText = `hidden: true`;
+    item.addEventListener('click', () => {
+        console.log('trash');
+    })
     
-// }
-// btn.addEventListener('click', deleteElement);
+});
 
-// btns.forEach(item => {
-//     item.addEventListener('click', deleteElement);
-// });
 
-// btns.forEach(item => {
-//     item.addEventListener('click', deleteElement, {once: true}); //work only once
-// });
-
-// btn.addEventListener('click', (e) => {
-//     e.preventDefault();
-//      console.log('hi');
-//  });
-
-// Navigation on DOM elements
-
-// console.log(document.documentElement); // we get all the elements of DOM
-// console.log(document.body); // we get all the elements of body etc.
-// console.log(document.body.childNodes); // we get all the nodes of body
-// console.log(document.body.firstChild);
-// console.log(document.body.lastChild);
-// console.log(document.body.firstElementChild); // getting the firs element of the child
-
-// now we can work with all elements of the page
-// console.log(document.querySelector('.add').parentNode.parentNode);
-// console.log(document.querySelector('[data-current = "3"]').previousSibling); // getting the previous node
-// console.log(document.querySelector('[data-current = "3"]').nextElementSibling); // getting the next element
-
-for(let node of document.body.childNodes) {
-    if(node.nodeName == '#text') {
-        continue;
+function addMovies(e) {
+    e.preventDefault();
+    if(input.value.length > 10) {
+        let a = input.value.split('').slice(0, 10).join('') + '...';
+        console.log(a);
+        movieDB.movies.push(a);
+        input.value = '';
+        
+    } else {
+        movieDB.movies.push(input.value);
+        input.value = '';
+        
     }
-    console.log(node);
+    start();
 }
+
+btn.addEventListener('click', addMovies);
+// checkbox.addEventListener('change', () => {
+//     if(checkbox.value) {
+//         console.log('hi');
+//     } else {
+//         console.log('by');
+//     }
+// });
+
+// movieDB.movies.sort();
+
+// list.innerHTML = '';
+
+function start() {
+    movieDB.movies.sort();
+
+    list.innerHTML = '';
+    movieDB.movies.forEach((item, i) => {
+        list.innerHTML += `
+            <li class="promo__interactive-item">${i + 1}. ${item}
+                <div class="delete"></div>
+            </li>
+            `
+    });
+    if(!checkbox.checked) {
+        console.log('not a favorite movie');
+    } else {
+        console.log('add your favorite movie');
+    }
+}
+start();
+// movieDB.movies.forEach((item, i) => {
+//     list.innerHTML += `
+//         <li class="promo__interactive-item">${i + 1}. ${item}
+//             <div class="delete"></div>
+//         </li>
+//         `
+// });
+
+adv.forEach(item => {
+    item.remove();
+});
+
+genre[0].textContent = 'Драма';
+promoBg[0].style.backgroundImage = 'url("img/bg.jpg")';
