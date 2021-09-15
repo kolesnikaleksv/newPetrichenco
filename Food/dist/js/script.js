da71041b6b88added1c428ccec350f88d25462b3
Alex <kolesnikaleksv@gmail.com>
2021-09-15 15:19:53 +0300

    lesson - Scripts and their execution time(setTimeout, setInterval)

--- Food/dist/js/script.js
+++ Food/dist/js/script.js
@@ -14,7 +14,6 @@ document.addEventListener('DOMContentLoaded', () => {
         tabContent.forEach(item => {
             item.classList.add('hide');
             item.classList.remove('show', 'fade');
-            //item.style.display = 'none';
         });
     }
     hideTabsContent();
@@ -23,7 +22,6 @@ document.addEventListener('DOMContentLoaded', () => {
         tabContent[i].classList.add('show', 'fade');
         tabContent[i].classList.remove('hide');
         tabs[i].classList.add('tabheader__item_active');
-        // tabContent[i].style.display = 'block';
     }
     showTabsContent();
 
@@ -41,17 +39,76 @@ document.addEventListener('DOMContentLoaded', () => {
             })            
         }
     })
-    // tabs.forEach( (item, i) => {
-    //     item.addEventListener('click', (e) => {
-    //         e.preventDefault();
-            
-    //         if(e.target && e.target.classList.contains('tabheader__item')) {
-    //             hideTabsContent();
-    //             showTabsContent(i);
-    //         } else {
-    //             hideTabsContent(i);
-    //         }
-            
-    //     })
-    // })
+    
 });
+//Scripts and their execution time (setTimeout, setInterval)
+
+// const timerId = setTimeout(function()  {
+//     console.log('hello');
+// }, 2000);
+
+// const timerId = setTimeout(function(text)  {
+//     console.log(text);
+// }, 2000, 'hello');
+
+// const timerId = setTimeout(logger, 2000);
+
+// function logger() {
+//     console.log('hello');
+// }
+
+// setTimeout(logger, 2000);// const timerId - это идентификатор определенного таймера
+
+// function logger() {
+//     console.log('hello');
+// }
+
+// clearInterval(timerId); // отключаем определенный таймер по его id
+
+const button = document.querySelector('.btn_white');
+let timerId,
+    i = 0;
+
+// button.addEventListener('click', () => {
+//     timerId = setTimeout(logger, 2000);
+//     //timerId = setInterval(logger, 2000);
+// });
+
+// clearInterval(timerId);
+
+// function logger() {
+//     console.log('hello');
+// }
+// button.addEventListener('click', () => {
+//     //timerId = setTimeout(logger, 2000);
+//     timerId = setInterval(logger, 2000);
+// });
+
+// function logger() {
+//     if(i === 3) {
+//         clearInterval(timerId);
+//     }
+//     console.log('hello');
+//     i++;
+// }
+//рекурсивный вызов setTimeout
+// let id = setTimeout(function log() {
+//     console.log('hello');
+//     id = setTimeout(log, 500);
+// },500);
+// простая анимация
+function myAnimation() {
+    const box = document.querySelector('.box');
+    let pos = 0;
+    const id = setInterval(frame, 10);
+    function frame() {
+        if(pos === 300) {
+            clearInterval(id);
+        } else {
+            pos++;
+            box.style.top = pos + 'px';
+            box.style.left = pos + 'px';
+        }
+    }
+}
+button.addEventListener('click', myAnimation);
