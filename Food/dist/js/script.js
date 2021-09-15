ff32e14cc45fededeaaedf428893984f4479b5bd
Alex <kolesnikaleksv@gmail.com>
2021-09-15 13:02:16 +0300

    The second option to creating tabs now through classes

--- Food/dist/js/script.js
+++ Food/dist/js/script.js
@@ -12,39 +12,33 @@ document.addEventListener('DOMContentLoaded', () => {
         });
 
         tabContent.forEach(item => {
-            item.style.display = 'none';
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+            //item.style.display = 'none';
         });
     }
     hideTabsContent();
 
     function showTabsContent(i = 0) {
+        tabContent[i].classList.add('show', 'fade');
+        tabContent[i].classList.remove('hide');
         tabs[i].classList.add('tabheader__item_active');
-        tabContent[i].style.display = 'block';
+        // tabContent[i].style.display = 'block';
     }
     showTabsContent();
 
-    
-    // tabItem.forEach((item, i) => {
-    //     console.log(`${item} and ${i}`);
-    //     if(item.classList.contains('tabheader__item_active') || i == 0) {
-    //         item.classList.remove('tabheader__item_active');
-    //         console.log('by');
-    //     } else {
-
-    //     }
-    // });
     tabsParent.addEventListener('click', e => {
         e.preventDefault();
-        console.log('hi');
+
         if(e.target && e.target.classList.contains('tabheader__item')) {
             const target = e.target;
+
             tabs.forEach((item, i) => {
                 if(target == item) {
                     hideTabsContent();
                     showTabsContent(i);
                 }
-            })
-                        
+            })            
         }
     })
     // tabs.forEach( (item, i) => {
