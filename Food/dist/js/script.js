aadb38b1ab721ab4d3e74af9daeb59987c7ff21a
Alex <kolesnikaleksv@gmail.com>
2021-09-18 14:10:07 +0300

    The lesson - The classes(ES6)

--- Food/dist/js/script.js
+++ Food/dist/js/script.js
@@ -145,188 +145,46 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     window.addEventListener('scroll', openModalOffset);
 });
-// The constructions function
 
-// const num = new Number(3);//outdated syntax
-// console.log(num);
+// The classes
+// The class is a beautiful cover for the constructor functions
+class Rectangle {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+    }
 
-//The modern way
-// function User(name, id) {
-//     this.name = name;
-//     this.id = id;
-//     this.human = true;
-//     this.hello = function() {
-//         console.log(`hello i am ${this.name} my age is ${this.id} Am i a human? ${this.human}`)
-//     }
-// }
+    square() {
+        return this.width * this.height;
+    }
+}
 
-// const alex = new User('alex', 20);
-// const kir = new User('kir', 9);
+const right = new Rectangle(25,25);
+const long = new Rectangle(25,250);
 
-// console.log(alex.hello(), kir.hello());
-// // У цей конструктор можна додати і інші функціі за рахунок наслідування
-// User.prototype.exit = function() {
-//     console.log(`${this.name} logged out`);
-// }
-// alex.exit();
-// kir.exit();
-// зараз у js користуються тільки классами - синтаксичний цукор
+console.log(right.square());
+console.log(long.square());
 
+//наследование от класса 
+// для этого мы прописываем extends и от кого наследуемся
+// для получения всех свойств родителя мі прописыываем метод super() и тогда нам не
+// надо копировать предыдущие свойства и да, супер всегда идет первой строкой и мы
+// можем указать те свойства которые хотим использовать
 
 
-// lesson two
-// Контекст вызова This
+class ColoredRectanglWithText extends Rectangle {
+    constructor (width, height, color, text) {
+        super(width, height);
+        this.color = color;
+        this.text = text;
+    }
 
-// function showThis(a,b) {
-//     console.log(this);
-// }
-// showThis();
-// 1) В обычной функции this = window при "use strict"; = undefined
-// function showThis(a,b) {
-//     console.log(this);
-//     function sum() {
-//         console.log(this);
-//         return this.a + this.b;
-//     }
-//     console.log(sum());
-// }
-// showThis(5,4);
-// ничего не получится, ответ один undefined что бы случились вычисления, надо использо
-//вать замыкание, тогда переменные будут искаться выше т.е. убрать this
-// function showThis(a,b) {
-//     console.log(this);
-//     function sum() {
-//         console.log(this);
-//         return a + b;
-//     }
-//     console.log(sum());
-// }
-// showThis(4,5);
-//2) Контекст вызова метода внутри объекта будет иметь контекстом - сам объект
+    showMyProps() {
+        console.log(this);
+        console.log(`my height is ${this.height} my width is ${this.width} my squere is ${this.square()}`);
+    }
+}
 
-// const obj = {
-//     'name': 'alex',
-//     'age': 25,
-//     sum:function  () {
-//         console.log(this);
-//     }
-// }
-// obj.sum();
-// если мы вызовем внутри метода функцию то она уже не будет иметь контекста
-// const obj = {
-//     'name': 'alex',
-//     'age': 25,
-//     sum:function  () {
-//         function hello() {
-//             console.log(this);
-//         }
-//         hello();
-//     }
-// }
-// obj.sum();
-// 3) this в конструкторах и классах это новый экзэмпляр объекта
-// function User(name, id) {
-//     this.name = name;
-//     this.id = id;
-//     this.human = true;
-//     this.hello = function() {
-//         console.log(this);
-//         console.log(`hello i am ${this.name} my age is ${this.id} Am i a human? ${this.human}`)
-//     }
-// }
-
-// const alex = new User('alex', 20);
-// const kir = new User('kir', 9);
-
-// console.log(alex.hello(), kir.hello());
-// Можно передать контекст в функцию с помощью метода call() и apply()
-// function sayName() {
-//     console.log(this);
-//     console.log(this.name);
-// }
-
-// const obj = {
-//     'name': 'alex'
-// }
-
-// sayName.call(obj);
-// sayName.apply(obj);
-// разница между ними только в записи аргументов при передаче аргументов
-// function sayName(surname) {
-//     console.log(this);
-//     console.log(this.name + surname);
-// }
-
-// const obj = {
-//     'name': 'alex'
-// }
-
-// sayName.call(obj, 'smith'); //все последующие аргументы через запятую
-// sayName.apply(obj, ['smith']);// все аргументы в виде массива
-// есть еще одни метод bind() но он уже создает новую ф-цию и под нее подвязывает контекст
-// function count(num) {
-//     return this*num;
-// }
-
-// const double = count.bind(2);
-// // т.е. теперь double это новая фция с новый контекстом 2 который передается в фцию count
-// console.log(double(3));
-// console.log(double(9));
-//т.е. 4) Ручная привязка this это - call() apply() bind()
-
-//for example
-//рассмотрим на примере событий
-const btn = document.querySelector('.btn_min');
-
-// btn.addEventListener('click', function() {
-//     console.log(this);
-//     this.style.color = 'red';
-// });
-//внутри событий в обычной callback функции контекст(this) будет равент e.target т.е.
-//самому єлементу на котором было вызвано событие
-
-// разберем стрелочную фцию у которой нет своего контекста вызова и она берет его у
-// своего родителя
-// const obj = {
-//     'name': 'alex',
-//     sayName: function() {
-//         console.log(this);
-//         const say = () => {
-//             console.log(this);
-//             const bay = () => {
-//                 console.log(this.name);
-//             }
-//             bay();
-//         }
-//         say();
-//     }
-// }
-// obj.sayName();
-//поскольку стрелочная фция берет контекст у родителя то все они сработают нормально
-// и this/контекстом будет сам объект. потому как метода объекта имеет контекстом сам
-// объект
-
-// синтаксис стрелочных фций
-// const double = (a) => {
-//     return a*2;
-// }
-// // равно нижней записи
-// const double = (a) => a*2;
-// равно нижней записи
-// const double = a => a*2;
-
-// console.log(double(3));
-// это кстати та фция с которой мы начинали.
-
-// теперь контекст вызова стрелочных фций в обработчике событий
-// btn.addEventListener('click', () => {
-//     console.log(this);
-//     this.style.color = 'red';
-// });
-// в обработчиках событий контекст будет потерян. Потому как только обычная фция имеет
-// в них контекст.
-// поэтому в обработчики мы передаем event и вместо this цепляем все к e.target
-btn.addEventListener('click', (e) => {
-    //console.log(this);
-    e.target.style.color = 'red';
-});
+const bar = new ColoredRectanglWithText(3,20);
+console.log(bar.square());
+bar.showMyProps();
