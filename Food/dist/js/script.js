75fbd73fab83042b396d22a1cafe36ded69c055f
Alex <kolesnikaleksv@gmail.com>
2021-09-27 15:27:10 +0300

    The second way to create a slider for the site from the teacher

--- Food/dist/js/script.js
+++ Food/dist/js/script.js
@@ -327,50 +327,80 @@ document.addEventListener('DOMContentLoaded', () => {
     //     slides[slideIndex - 1].classList.remove('hide');
     //     current.innerHTML = `${getZero(slideIndex)}`;
     // }
-
-    let slideIndex = 1;
+    
+// Second way to create the slider from teacher
+    
     const slides = document.querySelectorAll('.offer__slide'),
         prev = document.querySelector('.offer__slider-prev'),
         next = document.querySelector('.offer__slider-next'),
         total = document.querySelector('#total'),
-        current = document.querySelector('#current');
+        current = document.querySelector('#current'),
+        slideWrapper = document.querySelector('.offer__slider-wrapper'),
+        slidesField = document.querySelector('.offer__slider-inner'),
+        width = window.getComputedStyle(slideWrapper).width; //the object is returned. we get only the width
         
-    showSlides(slideIndex);
-    if (slides.length < 10) {
-        total.textContent = `0${slides.length}`;
-    } else {
-        total.textContent = slides.length;
-    }
+        let slideIndex = 1;
+        let offset = 0; // offset size
 
-    function showSlides(n) {
-        if (n > slides.length) {
-            slideIndex = 1;
-        }
-        if (n < 1) {
-            slideIndex = slides.length;
-        }
-
-        slides.forEach((item) => item.style.display = 'none');
-
-        slides[slideIndex - 1].style.display = 'block'; // Как ваша самостоятельная работа - переписать на использование классов show/hide
-        
-        if (slides.length < 10) {
-            current.textContent =  `0${slideIndex}`;
+        if(slides.length < 10) {
+            total.textContent = `0${slides.length}`;
+            current.textContent = `0${slideIndex}`;
         } else {
-            current.textContent =  slideIndex;
+            total.textContent = slides.length;
+            current.textContent = slideIndex;
         }
-    }
 
-    function plusSlides (n) {
-        showSlides(slideIndex += n);
-    }
-
-    prev.addEventListener('click', function(){
-        plusSlides(-1);
+    slidesField.style.width = 100 * slides.length + '%'; // we line up all the slides in one row
+    slidesField.style.display = 'flex'; // in a row
+    slidesField.style.transition = '0.5s all'; 
+    slideWrapper.style.overflow = 'hidden';
+    
+    slides.forEach(item => {
+        item.style.width = width;//give all slides the width of the window
     });
 
-    next.addEventListener('click', function(){
-        plusSlides(1);
+    next.addEventListener('click', () => {
+        if(offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {// 500px
+            offset = 0;
+        } else {
+            offset += +width.slice(0, width.length - 2);
+        }
+        slidesField.style.transform = `translateX(-${offset}px)`;
+
+        if(slideIndex == slides.length) {
+            slideIndex = 1;
+        } else {
+            slideIndex++;
+        }
+
+        if(slides.length < 10) {
+            current.textContent = `0${slideIndex}`;
+        } else {
+            current.textContent = slideIndex;
+        }
     });
+
+    prev.addEventListener('click', () => {
+        if(offset == 0) {// 500px
+            offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+        } else {
+            offset -= +width.slice(0, width.length - 2);
+        }
+        slidesField.style.transform = `translateX(-${offset}px)`;
+
+        if(slideIndex == 1) {
+            slideIndex = slides.length;
+        } else {
+            slideIndex--;
+        }
+    
+        if(slides.length < 10) {
+            current.textContent = `0${slideIndex}`;
+        } else {
+            current.textContent = slideIndex;
+        }
+    });
+    
+    
     
 });
